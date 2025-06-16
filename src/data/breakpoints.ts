0be805c43067e30469377ffcddfda7699acd476e
Daniel <135237553+dmaurelc@users.noreply.github.com>
2025-06-16 13:57:39 -0400

    refactor: actualizar formato de comillas en breakpoints.ts

--- src/data/breakpoints.ts
+++ src/data/breakpoints.ts
@@ -1,7 +1,7 @@
-import { Breakpoint } from '../types/config';
+import { Breakpoint } from "../types/config";
 
 export const breakpoints: Breakpoint[] = [
-  { width: 1280, label: 'Laptop' },
-  { width: 767, label: 'Tablet' },
-  { width: 480, label: 'Mobile' }
+  { width: 1280, label: "Laptop" },
+  { width: 767, label: "Tablet" },
+  { width: 480, label: "Mobile" },
 ];
